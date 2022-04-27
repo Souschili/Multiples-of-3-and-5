a8ffa5f54a92670615310bc4eff10ba51855077b
OrHaN_1982 <aliyevorhan82@gmail.com>
2022-04-28 01:19:48 +0200

    Clean comments and bad code

--- Multiples of 3 and 5/Program.cs
+++ Multiples of 3 and 5/Program.cs
@@ -11,33 +11,21 @@ namespace Multiples_of_3_and_5
         static void Main(string[] args)
         {
             int n = 10;
-            int min = 5;
-            //Console.WriteLine(Div(n,5));
-            //Console.WriteLine(Div(n,3));
-            //Console.WriteLine(Div(n,15));
 
-            Console.WriteLine(Div(n,15));
-            Console.WriteLine(Div(n,5));
-            Console.WriteLine(Div(n,3));
-            Console.WriteLine(Div(n,3)+Div(n,5)-Div(n,15));
 
-            //BigInteger count = (n - 1) / min;
-            //BigInteger maxVal = min + ((count - 1) * min);
-            //BigInteger summ = ((min + maxVal) / 2) * count;
-
-            //Console.WriteLine($"count ={count} maxVal= {maxVal}   summ= {summ}");
-            //1683   =3
-            //312    =15
-            //950    =5
+            Console.WriteLine(Div(n, 15));
+            Console.WriteLine(Div(n, 5));
+            Console.WriteLine(Div(n, 3));
+            Console.WriteLine(Div(n, 3) + Div(n, 5) - Div(n, 15));
 
 
         }
 
-        static BigInteger Div(BigInteger n,int min)
+        static BigInteger Div(BigInteger n, int min)
         {
             BigInteger count = (n - 1) / min;
-            BigInteger maxVal=min+((count-1)* min);
-            BigInteger summ=((min+maxVal)*count)/2;
+            BigInteger maxVal = min + ((count - 1) * min);
+            BigInteger summ = ((min + maxVal) * count) / 2;
             return summ;
         }
 
